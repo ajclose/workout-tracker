6cf9791b65c03535a923a1eca5b8973534e20b17
Alex Close <ajclose@gmail.com>
2017-09-16 14:47:44 -0500

    create interface for tell me what I have

--- app/assets/javascripts/weight_calculator.js
+++ app/assets/javascripts/weight_calculator.js
@@ -63,54 +63,64 @@ function weightForm() {
   <input type="radio" name="barWeight" id="barWeight_45" value="45" checked="checked">
   <label for="barWeight">45 lbs</label>
   <div>
+  </form>
 
   <div class="">
   <p>Click the weight to add it to the bar</p>
-  <div class="weight" id="45">
-  45
+  <div class="plateRack">
+  <div class="stack">
+    <div class="weight horizontal-plate-45">45</div>
+    <div class="weight horizontal-plate-45">45</div>
+    <div class="weight horizontal-plate-45">45</div>
+    <div class="weight horizontal-plate-45">45</div>
+    <div class="weight horizontal-plate-45">45</div>
+  </div>
+    <div class="stack">
+      <div class="weight horizontal-plate-25">25</div>
+      <div class="weight horizontal-plate-25">25</div>
+      <div class="weight horizontal-plate-25">25</div>
+      <div class="weight horizontal-plate-25">25</div>
+      <div class="weight horizontal-plate-25">25</div>
+    </div>
+
+    <div class="stack">
+      <div class="weight horizontal-plate-15">15</div>
+      <div class="weight horizontal-plate-15">15</div>
+      <div class="weight horizontal-plate-15">15</div>
+      <div class="weight horizontal-plate-15">15</div>
+      <div class="weight horizontal-plate-15">15</div>
+    </div>
+
+    <div class="stack">
+      <div class="weight horizontal-plate-10">10</div>
+      <div class="weight horizontal-plate-10">10</div>
+      <div class="weight horizontal-plate-10">10</div>
+      <div class="weight horizontal-plate-10">10</div>
+      <div class="weight horizontal-plate-10">10</div>
+    </div>
+
+    <div class="stack">
+      <div class="weight horizontal-plate-5">5</div>
+      <div class="weight horizontal-plate-5">5</div>
+      <div class="weight horizontal-plate-5">5</div>
+      <div class="weight horizontal-plate-5">5</div>
+      <div class="weight horizontal-plate-5">5</div>
+    </div>
+
+    <div class="stack">
+      <div class="weight horizontal-plate-2-5">2.5</div>
+      <div class="weight horizontal-plate-2-5">2.5</div>
+      <div class="weight horizontal-plate-2-5">2.5</div>
+      <div class="weight horizontal-plate-2-5">2.5</div>
+      <div class="weight horizontal-plate-2-5">2.5</div>
+    </div>
+
+
+  </div>
   </div>
 
-  <div class="weight" id="25">
-  25
-  </div>
-
-  <div class="weight" id="15">
-  15
-  </div>
-
-  <div class="weight" id="10">
-  10
-  </div>
-
-  <div class="weight" id="5">
-  5
-  </div>
-
-  <div class="weight" id="2.5">
-  2.5
-  </div>
-  </div>
-
-</form>
   `
 }
-
-function getBarWeight(barWeightSelections) {
-  for (var i = 0; i < barWeightSelections.length; i++) {
-    const barWeightSelection = barWeightSelections[i]
-    if (barWeightSelection.checked) {
-      barWeight = barWeightSelection.value
-      console.log(barWeight);
-    }
-    barWeightSelection.addEventListener("change", function() {
-      barWeight = barWeightSelection.value
-      console.log(barWeight);
-    })
-  }
-  return barWeight
-}
-
-
 
 document.addEventListener("DOMContentLoaded", function(event) {
   const results = document.querySelector(".results")
@@ -122,9 +132,77 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const weights = []
   let barWeight;
 
-  plateButton.addEventListener("click", function(event) {
+  function getBarWeight(barWeightSelections, weights) {
+    for (var i = 0; i < barWeightSelections.length; i++) {
+      const barWeightSelection = barWeightSelections[i]
+      if (barWeightSelection.checked) {
+        barWeight = barWeightSelection.value
+      }
+      barWeightSelection.addEventListener("change", function() {
+        barWeight = barWeightSelection.value
+        if (document.querySelector("form.weight")) {
+          totalWeight(barWeight, weights)
+        }
+      })
+    }
+    return barWeight
+  }
+
+  function totalWeight(barWeight, weights) {
+    let totalWeight = parseInt(barWeight)
+    for (var i = 0; i < weights.length; i++) {
+      const weight = weights[i]
+      totalWeight += weight * 2
+    }
+    const html = `
+    You have ${totalWeight} pounds!
+    `
+    results.textContent = ""
+    results.insertAdjacentHTML("beforeend", html)
+    buildBar(weights)
+  }
+
+  function clearPage() {
     formDiv.textContent = ""
     results.textContent = ""
+    barLeft.textContent = ""
+    barRight.textContent = ""
+  }
+  function buildBar(weights) {
+    barLeft.textContent = ""
+    barRight.textContent = ""
+    for (var i = 0; i < weights.length; i++) {
+      const weight = weights[i]
+      if (weight == 2.5) {
+        const html = `
+        <div class="barPlate plate-2-5">2.5</div>
+        `
+        barLeft.insertAdjacentHTML("afterbegin", html)
+        barRight.insertAdjacentHTML("beforeend", html)
+      } else {
+        const html = `
+        <div class="barPlate plate-${weight}">${weight}</div>
+        `
+        barLeft.insertAdjacentHTML("afterbegin", html)
+        barRight.insertAdjacentHTML("beforeend", html)
+      }
+    }
+    const barPlates = document.querySelectorAll(".barPlate")
+    if (barPlates) {
+      for (var i = 0; i < barPlates.length; i++) {
+        const barPlate = barPlates[i]
+        barPlate.addEventListener("click", function(event) {
+          const barPlateWeight = barPlate.textContent
+          const plateIndex = weights.indexOf(barPlateWeight)
+          weights.splice(plateIndex, 1)
+          totalWeight(barWeight, weights)
+        })
+      }
+    }
+  }
+
+  plateButton.addEventListener("click", function(event) {
+    clearPage()
     const form = plateForm()
     formDiv.insertAdjacentHTML("afterbegin", form)
     const weightSelections = document.querySelectorAll('input[name="weight"]')
@@ -133,33 +211,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
         const weightSelection = weightSelections[i]
         if(weightSelection.checked) {
           weights.push(weightSelection.value)
-          console.log(weights);
-        }
+          }
         weightSelection.addEventListener("change", function() {
           if (!this.checked) {
             const index = weights.indexOf(this.value)
             weights.splice(index, 1)
-            console.log(weights);
-          } else {
+              } else {
             weights.push(this.value)
-            console.log(weights);
-          }
+              }
         })
       }
     }
     const barWeightSelections = document.querySelectorAll('input[name="barWeight"]')
     if (barWeightSelections) {
-      for (var i = 0; i < barWeightSelections.length; i++) {
-        const barWeightSelection = barWeightSelections[i]
-        if (barWeightSelection.checked) {
-          barWeight = barWeightSelection.value
-          console.log(barWeight);
-        }
-        barWeightSelection.addEventListener("change", function() {
-          barWeight = barWeightSelection.value
-          console.log(barWeight);
-        })
-      }
+      barWeight = getBarWeight(barWeightSelections)
     }
 
     const platesForm = document.querySelector('form.plates')
@@ -170,7 +235,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
         barRight.textContent = ""
         const totalWeight = document.querySelector('input[name="totalWeight"]').value
         let plates = calcWeights(totalWeight, weights, barWeight)
-        console.log(plates);
         for(var weight in plates) {
           for (var i = 0; i < plates[weight]/2; i++) {
             if (weight == 2.5) {
@@ -209,48 +273,26 @@ document.addEventListener("DOMContentLoaded", function(event) {
   })
 
   weightButton.addEventListener("click", function(event) {
-    formDiv.textContent = ""
-    results.textContent = ""
+    clearPage()
     const form = weightForm()
     const weights = []
     formDiv.insertAdjacentHTML("afterbegin", form)
     const barWeightSelections = document.querySelectorAll('input[name="barWeight"]')
-    for (var i = 0; i < barWeightSelections.length; i++) {
-      const barWeightSelection = barWeightSelections[i]
-      if (barWeightSelection.checked) {
-        barWeight = barWeightSelection.value
-        console.log(barWeight);
-        totalWeight(barWeight, weights)
-      }
-      barWeightSelection.addEventListener("change", function() {
-        barWeight = barWeightSelection.value
-        console.log(barWeight);
-        totalWeight(barWeight, weights)
-      })
+    if (barWeightSelections) {
+      barWeight = getBarWeight(barWeightSelections, weights)
+      totalWeight(barWeight, weights)
     }
+
     const plates = document.querySelectorAll("div.weight")
     if (plates) {
       for (var i = 0; i < plates.length; i++) {
         const plate = plates[i]
         plate.addEventListener("click", function(event) {
-          const plateWeight = parseFloat(plate.textContent) * 2
-          console.log(plateWeight);
-          weights.push(plateWeight)
+          const weight = plate.textContent
+          weights.push(weight)
           totalWeight(barWeight, weights)
         })
       }
-    }
-    function totalWeight(barWeight, weights) {
-      let totalWeight = parseInt(barWeight)
-      for (var i = 0; i < weights.length; i++) {
-        const weight = weights[i]
-        totalWeight += weight
-      }
-      const html = `
-      You have ${totalWeight} pounds!
-      `
-      results.textContent = ""
-      results.insertAdjacentHTML("beforeend", html)
     }
   })
 })
