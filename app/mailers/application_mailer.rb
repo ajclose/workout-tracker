6e523ecbf9b466cefd2fc8c17787e9c5826a39c2
Alex Close <ajclose@gmail.com>
2017-11-10 18:55:18 -0600

    Fix update password

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'noreply@ajclose-wodtracker.herokuapp.com/'
+  default from: 'noreply@ajclose-wodtracker.herokuapp.com'
   layout 'mailer'
 end
